ab7277f5e8c8fd0b508ba186fe194705924fbb3d
sdeleonc7 <Sdeleonc7@miumg.edu.gt>
2026-04-08 15:30:00 -0600

    feat(flutter): implementar pantalla de registro de pago manual

--- src/pagos/pagos.module.ts
+++ src/pagos/pagos.module.ts
@@ -10,4 +10,4 @@ import { Pago } from '../entities/pago.entity';
   providers: [PagoService],
   exports: [PagoService],
 })
-export class PagoModule {}
+export class PagoModule {}// Pantalla registro pago flutter verificada con endpoints
