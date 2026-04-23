66fcb14fda4b9de4c2ca81f3a658029620b6151b
sdeleonc7 <Sdeleonc7@miumg.edu.gt>
2026-04-23 14:00:00 -0600

    feat(flutter): implementar pantalla de gestion de sectores y asignacion

--- src/incidencias/incidencias.module.ts
+++ src/incidencias/incidencias.module.ts
@@ -10,4 +10,4 @@ import { Incidencia } from '../entities/incidencia.entity';
   providers: [IncidenciaService],
   exports: [IncidenciaService],
 })
-export class IncidenciaModule {}
+export class IncidenciaModule {}// Gestion sectores flutter: CRUD sectores y asignacion hogares
