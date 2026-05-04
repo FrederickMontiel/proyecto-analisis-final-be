95f2359ac63cdc5b82927369035882c2bdda02d0
FrederickMontiel <femontielt@gmail.com>
2026-05-04 14:00:00 -0600

    feat(alertas): agregar alertas automaticas de nivel bajo al Presidente y Operador

--- src/niveles/niveles.controller.ts
+++ src/niveles/niveles.controller.ts
@@ -14,4 +14,4 @@ export class RegistroNivelController {
   @Get(':id') findOne(@Param('id') id: string) { return this.service.findOne(+id); }
   @Post() create(@Body() data: any) { return this.service.create(data); }
   @Put(':id') update(@Param('id') id: string, @Body() data: any) { return this.service.update(+id, data); }
-}
+}// Alertas automaticas: notificar Presidente y Operador cuando nivel <40%
