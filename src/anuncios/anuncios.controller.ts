163df0d484ebc625eda46ab1f16cc75e7a9cddb9
sdeleonc7 <Sdeleonc7@miumg.edu.gt>
2026-05-05 09:30:00 -0600

    feat(flutter): implementar pantalla de anuncios comunitarios con tipos coloreados

--- src/anuncios/anuncios.controller.ts
+++ src/anuncios/anuncios.controller.ts
@@ -14,4 +14,4 @@ export class AnuncioController {
   @Get(':id') findOne(@Param('id') id: string) { return this.service.findOne(+id); }
   @Post() create(@Body() data: any) { return this.service.create(data); }
   @Put(':id') update(@Param('id') id: string, @Body() data: any) { return this.service.update(+id, data); }
-}
+}// Pantalla anuncios flutter: tipos con colores Urgente=rojo, Mantenimiento=naranja
