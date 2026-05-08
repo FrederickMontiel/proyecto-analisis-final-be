3b9c72b91ffc4953971a73e7218a0dc9f811b846
FrederickMontiel <femontielt@gmail.com>
2026-05-08 10:00:00 -0600

    feat(flutter): implementar pantalla de parametrizacion del sistema por categorias

--- src/parametros/parametros.controller.ts
+++ src/parametros/parametros.controller.ts
@@ -14,4 +14,4 @@ export class ParametrosSistemaController {
   @Get(':id') findOne(@Param('id') id: string) { return this.service.findOne(+id); }
   @Post() create(@Body() data: any) { return this.service.create(data); }
   @Put(':id') update(@Param('id') id: string, @Body() data: any) { return this.service.update(+id, data); }
-}
+}// Pantalla parametrizacion flutter: grupos por categoria con validacion tipo dato
