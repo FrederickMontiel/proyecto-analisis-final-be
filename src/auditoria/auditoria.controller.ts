0079526fa75008fe62bc7e218c14b70e1ed75a10
FrederickMontiel <femontielt@gmail.com>
2026-05-10 11:00:00 -0600

    feat(flutter): implementar pantalla de log de auditoria con filtros

--- src/auditoria/auditoria.controller.ts
+++ src/auditoria/auditoria.controller.ts
@@ -14,4 +14,4 @@ export class LogAuditoriaController {
   @Get(':id') findOne(@Param('id') id: string) { return this.service.findOne(+id); }
   @Post() create(@Body() data: any) { return this.service.create(data); }
   @Put(':id') update(@Param('id') id: string, @Body() data: any) { return this.service.update(+id, data); }
-}
+}// Log auditoria flutter: tabla filtrable por usuario, tabla, fecha, exportar Excel
